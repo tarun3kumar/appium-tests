a122094c51d0c43a1a4fc14d252871af963d148c
tarun.kumar.bhadauria <tarun.kumar.bhadauria@raisin.com>
2017-01-04 15:45:55 +0100

    update capabilities

--- sample-code/examples/java/junit/src/test/java/com/saucelabs/appium/AndroidEmulatorWebTest.java
+++ sample-code/examples/java/junit/src/test/java/com/saucelabs/appium/AndroidEmulatorWebTest.java
@@ -2,6 +2,7 @@ package com.saucelabs.appium;
 
 import java.net.URL;
 
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,15 +22,19 @@ public class AndroidEmulatorWebTest {
     public void setUp() throws Exception {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("BROWSER_NAME", "Android");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
 
         // Use app browser when running test on emulator
-        capabilities.setCapability("app", "Browser");
+        //capabilities.setCapability(MobileCapabilityType.APP, "Browser");
 
         // Set up version and device name else appium server would pick the only available emulator/device
-        capabilities.setCapability("VERSION", "XXX");
-        capabilities.setCapability("deviceName", "XXX");
-        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability(MobileCapabilityType.VERSION, "XXX");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "XXX");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+
+        // Set version applicable for your device
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1");
+
 
         // 4723 is the port where appium server is running, you can see it when starting the appium server
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
